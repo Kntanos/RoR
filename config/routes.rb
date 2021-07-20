931abac8ead37413db68a974bf7c370ed5ed62e4
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-07-20 16:47:37 +0100

    1h31m in the video

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :contacts
+  get 'home/index'
+  get 'home/about'
 end
+  # root 'home#index' Comment our ln#2 and uncomment this one to direct localhost:3000 to the homepage without the need to type localhost:3000/home/index
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
