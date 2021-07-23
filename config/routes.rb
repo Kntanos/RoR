0ec8017903fde0f98025c3919818a37568890ef3
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-07-23 17:07:27 +0100

    Before Fun with Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :contacts
-  root 'home#index'
+  #root 'home#index'
+  root 'contacts#index'
   get 'home/about'
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
